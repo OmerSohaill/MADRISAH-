7a5be7f2639e267e332a6d5545d506cbcae50d3a
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-05-22 00:16:01 +0500

    c

--- src/com/contact.js
+++ src/com/contact.js
@@ -19,22 +19,31 @@ function Contact() {
             style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }} // Corrected inline style format
           ></iframe>
         </div>
+        <form action="http://localhost:3001/contactus" method='POST'>
+
+        
         <div className="container px-5 py-24 mx-auto flex">
           <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-            <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contact Us</h2>
+            <p className="leading-relaxed mb-5 text-gray-600">Enter your Email or Phone num or both and your meesage </p>
             <div className="relative mb-4">
               <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
               <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
             </div>
             <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-600">Phone</label>
+              <input type="number" id="" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            </div>
+            <div className="relative mb-4">
               <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
               <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
-            <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-            <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+            <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">send message</button>
+            <p className="text-xs text-gray-500 mt-3">Enter your email and your message  and click on send button we will contact you through email as soon as possible.</p>
           </div>
         </div>
+        </form>
+
       </section>
       <Subscrib/>
       <Footer/>
